c110b9d42d04ad21f84b330bc2b89f93f429d048
serafinebot <hexdhog@proton.me>
2026-09-15 21:54:17 +0200

    nvim - added jupynvim remote server

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -35,6 +35,31 @@ require("lazy").setup({
     },
   },
   {
+    "nosduco/remote-sshfs.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
+    },
+    cmd = {
+      "RemoteSSHFSConnect",
+      "RemoteSSHFSDisconnect",
+      "RemoteSSHFSEdit",
+      "RemoteSSHFSFindFiles",
+      "RemoteSSHFSLiveGrep",
+    },
+    keys = {
+      { "<leader>rc", function() require("remote-sshfs.api").connect() end, desc = "ssh: connect" },
+      { "<leader>rd", function() require("remote-sshfs.api").disconnect() end, desc = "ssh: disconnect" },
+      { "<leader>re", function() require("remote-sshfs.api").edit() end, desc = "ssh: edit config" },
+      { "<leader>rf", function() require("remote-sshfs.api").find_files() end, desc = "ssh: find files" },
+      { "<leader>rg", function() require("remote-sshfs.api").live_grep() end, desc = "ssh: live grep" },
+    },
+    config = function()
+      require("remote-sshfs").setup({})
+      require("telescope").load_extension("remote-sshfs")
+    end,
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
@@ -180,6 +205,12 @@ require("lazy").setup({
     config = function()
       require("jupynvim").setup({
         image_renderer = "placeholder",
+        remote = {
+          arch = {
+            host = "arch",
+            core_path = "~/.local/bin/jupynvim-core"
+          }
+        },
         keymaps = {
           run_advance = "<S-CR>",
           run_advance_alt = "<leader>nr",
